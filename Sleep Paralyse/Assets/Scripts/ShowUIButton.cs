5d7f45f5ef56a5a5e4ff2ee32cda8db5922b5937
grivel123 <65075998+grivel123@users.noreply.github.com>
2020-06-02 12:52:15 +0200

    tür plus quest

---
+++ Sleep Paralyse/Assets/Scripts/ShowUIButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUIButton : MonoBehaviour
+{
+
+    public GameObject uiObject;
+
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+
+    void OnTriggerStay(Collider plyr)
+    {
+        if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+        IEnumerator WaitForSec()
+        {
+            yield return new WaitForSeconds(5);
+            Destroy(uiObject);
+            Destroy(gameObject);
+        }
+
+
+}
