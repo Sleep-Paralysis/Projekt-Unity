8d43c99429874562c6846510bf6ed453b5a030f4
grivel123 <65075998+grivel123@users.noreply.github.com>
2020-06-03 11:52:00 +0200

    Door Mechanics
    -Door opener
    -press "E" fehlt noch die info

---
+++ Sleep Paralyse/Assets/Scripts/DoorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Door")
+        {
+            Animator anim = other.GetComponentInChildren<Animator>();
+            if (Input.GetKeyDown(KeyCode.E))
+                anim.SetTrigger("OpenClose");
+        }
+    }
+}
